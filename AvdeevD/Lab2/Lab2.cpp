208713371be372af1395951b3a44fa4f7f420b35
qwertewer <fasdewer500@gmail.com>
2025-03-11 20:09:00 +0300

    change_files

--- AvdeevD/Lab2/Lab2.cpp
+++ AvdeevD/Lab2/Lab2.cpp
@@ -8,7 +8,12 @@ public:
 		arr = new double[n];
 	}
 	DinArr(int n_) :
-		n(n_), arr(new double[n]) {};
+		n(n_), arr(nullptr) {
+		if (n_ <= 0) {
+			throw "Bad size.";
+		}
+		arr = new double[n];
+	};
 	void set_n_manual() {
 		int x;
 		std::cout << "Enter size of the array: " << std::endl;
@@ -17,56 +22,55 @@ public:
 		n = x;
 		double* arr_1 = new double[n];
 		for (int i = 0; i < n; i++) {
-			arr[i] = 0;
+			arr_1[i] = 0;
 		}
 		if (old_n < n) {
 			for (int i = 0; i < old_n; i++) {
 				arr_1[i] = arr[i];
 			}
-			arr = arr_1;
 		}
 		else {
 			for (int i = 0; i < n; i++) {
 				arr_1[i] = arr[i];
 			}
-			arr = arr_1;
 		}
-		delete[] arr_1;
+		delete[] arr;
+		arr = arr_1;
 	}
 	void set_n(int x) {
 		int old_n = n;
 		n = x;
 		double* arr_1 = new double[n];
 		for (int i = 0; i < n; i++) {
-			arr[i] = 0;
+			arr_1[i] = 0;
 		}
 		if (old_n < n) {
 			for (int i = 0; i < old_n; i++) {
 				arr_1[i] = arr[i];
 			}
-			arr = arr_1;
 		}
 		else {
 			for (int i = 0; i < n; i++) {
 				arr_1[i] = arr[i];
 			}
-			arr = arr_1;
 		}
-		delete[] arr_1;
+		delete[] arr;
+		arr = arr_1;
 	}
 	int get_n()const {
 		return n;
 	}
 	double get_itoe(int i)const {
-		if (i < n) {
+		if (i >= 0 && i < n) {
 			return arr[i];
 		}
 		else {
-			std::cout << "There is no such index for a number." << std::endl;
+			std::cout << "There is no such index for a number. The returning value will be 0." << std::endl;
+			return 0;
 		}
 	}
 	void set_itoe_manual(int i) {
-		if (i < n) {
+		if (i >= 0 && i < n) {
 			double itoe;
 			std::cout << "Enter the number: ";
 			std::cin >> itoe; 
@@ -77,7 +81,7 @@ public:
 		}
 	}
 	void set_itoe(int i, double itoe) {
-		if (i < n) {
+		if (i >= 0 && i < n) {
 			arr[i] = itoe;
 		}
 		else {
@@ -95,33 +99,37 @@ public:
 	}
 	void check_sort() {
 		int count = 0;
+		bool flag = true;
 		for (int i = 1; i < n; i++) {
-			if (arr[i - 1] < arr[i]) {
-				++count;
+			if (arr[i] < arr[i - 1]) {
+				flag = false;
+				break;
 			}
 		}
-		if (count == n - 1) {
-				std::cout << "The array is sorted." << std::endl;
+		if (flag) {
+			std::cout << "The array is sorted in ascending order." << std::endl;
 		}
 		else {
-			std::cout << "The array is not sorted." << std::endl;
+			flag = true;
+			for (int i = 1; i < n; i++) {
+				if (arr[i] > arr[i-1]) {
+					flag = false;
+				}
+			}
+			if (flag) {
+				std::cout << "The array is sorted in descending order." << std::endl;
+			}
+			else {
+				std::cout << "The array is not sorted." << std::endl;
+			}
 		}
 	}
 	DinArr not_odd() {
-		int not_odd_n = 0;
-		for (int i = 0; i < n; i++) {
-			if (i % 2 != 0) {
-				not_odd_n++;
-			}
-		}
+		int not_odd_n = floor(n/2);
 		DinArr res;
 		res.set_n(not_odd_n);
-		int count = 0;
-		for (int i = 0; i < n; i++) {
-			if (i % 2 != 0) {
-				res.set_itoe(count, arr[i]);
-				count++;
-			}
+		for (int i = 1; i < n; i+2) {
+			res.set_itoe(i, arr[i]);
 		}
 		return res;
 	}
